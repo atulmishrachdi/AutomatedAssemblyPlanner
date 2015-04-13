7413294c5ff7045d9dfab566684d29d67f2d7f2c
nima1367 <nima1367@users.noreply.github.com>
2015-04-13 12:19:25 -0700

    The direction generator class called icosahedron.cs is updated. It is working fast and the number of directions can be changed easily.

--- Assembly Planner/InitialGraphMaker/Icosahedron.cs
+++ Assembly Planner/InitialGraphMaker/Icosahedron.cs
@@ -23,18 +23,20 @@ namespace Assembly_Planner
                 new double[] {0, 0, -1},
                 new double[] {0, 0, 1}
             };
-
+            const int h1 = 2;
+            const int h2 = 1;
+            var stepSize = h2*1.0/(Math.Pow(10, h1));
             //for the plane [1,0,0]
             foreach (var plane in planes)
             {
                 if (plane[0] != 0)
                 {
                     var i = plane[0];
-                    for (double j = -1; j <= 1; j += 0.001)
+                    for (var j = -1.0; j <= 1.0; j += stepSize)
                     {
-                        for (double k = -1; k <= 1; k += 0.001)
+                        for (var k = -1.0; k <= 1.0; k += stepSize)
                         {
-                            var m = new[] { i, j, k };
+                            var m = new[] { Math.Round(i, h1), Math.Round(j, h1), Math.Round(k, h1) };
                             directions.Add(m.normalize());
                         }
                     }
@@ -42,11 +44,11 @@ namespace Assembly_Planner
                 if (plane[1] != 0)
                 {
                     var j = plane[1];
-                    for (double i = -1; i <= 1; i += 0.001)
+                    for (var i = -1.0; i <= 1.0; i += stepSize)
                     {
-                        for (double k = -1; k <= 1; k += 0.001)
+                        for (var k = -1.0; k <= 1.0; k += stepSize)
                         {
-                            var m = new[] { j, j, k };
+                            var m = new[] { Math.Round(i, h1), Math.Round(j, h1), Math.Round(k, h1) };
                             directions.Add(m.normalize());
                         }
                     }
@@ -54,12 +56,12 @@ namespace Assembly_Planner
                 if (plane[2] != 0)
                 {
                     var k = plane[2];
-                    for (double i = -1; i <= 1; i += 0.001)
+                    for (var i = -1.0; i <= 1.0; i += stepSize)
                     {
-                        for (double j = -1; j <= 1; j += 0.001)
+                        for (var j = -1.0; j <= 1.0; j += stepSize)
                         {
-                            var m = new[] { j, j, k };
-                            directions.Add(m.normalize());
+                            var m = new[] { Math.Round(i, h1), Math.Round(j, h1), Math.Round(k, h1) };
+                                directions.Add(m.normalize());
                         }
                     }
                 }
