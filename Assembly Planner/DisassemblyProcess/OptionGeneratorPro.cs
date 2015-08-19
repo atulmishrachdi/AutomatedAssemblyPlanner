0bd496722985c8ae8697cc8d85a35351785246bb
nima1367 <nima1367@users.noreply.github.com>
2015-08-19 15:16:17 -0700

    A new "OptionGenerator" code is written. It is still being tested.

--- Assembly Planner/DisassemblyProcess/OptionGeneratorPro.cs
+++ Assembly Planner/DisassemblyProcess/OptionGeneratorPro.cs
@@ -21,8 +21,8 @@ namespace Assembly_Planner
             //AddingOptionsToGraph(assemblyGraph, combinations, seperate.nodes.Count);
             options.AddRange(AddingOptionsToGraph(combinations, seperate, options, gOptions));
             var counter = 0;
-            var cp1 = new HashSet<HashSet<hyperarc>>();
-            var cp2 = new HashSet<HashSet<hyperarc>>();
+            var cp1 = new List<List<hyperarc>>();
+            var cp2 = new List<List<hyperarc>>();
 
             do
             {
@@ -36,19 +36,19 @@ namespace Assembly_Planner
                     {
                         if (opt.Contains(key) || blockingDic[key].Count==0) continue;
                         if (!blockingDic[key].All(bl => opt.Contains(bl))) continue;
-                        var newOp = new HashSet<hyperarc>(opt) { key };
+                        var newOp = new List<hyperarc>(opt) { key };
                         if (!cp1.Where(cp => cp.All(hy => newOp.Contains(hy))).Any(cp => newOp.All(cp.Contains)))
                             freeSCCs.Add(key);
                     }
 
                     if (freeSCCs.Count == 0) continue;
-                    combinations = CombinationsCreator(freeSCCs);
+                    combinations = CombinationsCreatorPro3(assemblyGraph, freeSCCs, opt);
                     var combAndPar = AddingParents(opt, combinations);
                     options.AddRange(AddingOptionsToGraph(combAndPar, seperate, options, gOptions));
-                    cp2.UnionWith(combAndPar);
+                    cp2.AddRange(combAndPar);
                 }
                 counter = 1;
-                cp1 = new HashSet<HashSet<hyperarc>>(cp2);
+                cp1 = new List<List<hyperarc>>(cp2);
             } while (cp1.Count > 0);
 
             foreach (var SCCHy in assemblyGraph.hyperarcs.Where(hyScc =>
@@ -59,10 +59,10 @@ namespace Assembly_Planner
             return options;
         }
 
-        private static HashSet<option> AddingOptionsToGraph(HashSet<HashSet<hyperarc>> combAndPar, hyperarc seperate,
+        private static List<option> AddingOptionsToGraph(List<List<hyperarc>> combAndPar, hyperarc seperate,
             List<option> options, List<option> gOptions)
         {
-            var optionList = new HashSet<option>();
+            var optionList = new List<option>();
             var sep = seperate.nodes.Count;
             foreach (var opt in combAndPar)
             {
@@ -88,9 +88,9 @@ namespace Assembly_Planner
             return optionList;
         }
 
-        private static HashSet<HashSet<hyperarc>> AddingParents(HashSet<hyperarc> opt, HashSet<HashSet<hyperarc>> combinations)
+        private static List<List<hyperarc>> AddingParents(List<hyperarc> opt, List<List<hyperarc>> combinations)
         {
-            var comb2 = new HashSet<HashSet<hyperarc>>();
+            var comb2 = new List<List<hyperarc>>();
             foreach (var c in combinations)
             {
                 foreach (var h in opt)
@@ -100,34 +100,34 @@ namespace Assembly_Planner
             return comb2;
         }
 
-        private static HashSet<HashSet<hyperarc>> CombinationsCreator(List<hyperarc> freeSCCs)
+        private static List<List<hyperarc>> CombinationsCreator(List<hyperarc> freeSCCs)
         {
-            var comb = new HashSet<HashSet<hyperarc>>();
+            var comb = new List<List<hyperarc>>();
             var lastGroup = new List<List<hyperarc>>();
             foreach (var hy in freeSCCs)
             {
                 lastGroup.Add(new List<hyperarc> { hy });
-                comb.Add(new HashSet<hyperarc>{hy});
+                comb.Add(new List<hyperarc> { hy });
             }
 
             var i = 0;
             while (i < freeSCCs.Count - 1)
             {
-                var newGroup = new HashSet<HashSet<hyperarc>>();
+                var newGroup = new List<List<hyperarc>>();
                 for (var j = 0; j < freeSCCs.Count - 1; j++)
                 {
                     var hy1 = freeSCCs[j];
                     for (var k = j + 1; k < lastGroup.Count; k++)
                     {
                         var hy2 = lastGroup[k];
-                        var com = new HashSet<hyperarc>{ hy1 };
-                        com.UnionWith(hy2);
+                        var com = new List<hyperarc> { hy1 };
+                        com.AddRange(hy2);
                         if ((newGroup.Any(hy => hy.All(com.Contains) && com.All(hy.Contains))) || hy2.Contains(hy1))
                             continue;
                         newGroup.Add(com);
                     }
                 }
-                comb.UnionWith(newGroup);
+                comb.AddRange(newGroup);
                 lastGroup.Clear();
                 lastGroup.AddRange(newGroup.Select(nG => nG.ToList()));
                 i++;
@@ -135,12 +135,12 @@ namespace Assembly_Planner
             return comb;
         }
 
-        private static HashSet<HashSet<hyperarc>> CombinationsCreatorPro(designGraph assemblyGraph, List<hyperarc> freeSCCs)
+        private static List<List<hyperarc>> CombinationsCreatorPro(designGraph assemblyGraph, List<hyperarc> freeSCCs)
         {
             // The combinations must meet these two conditions:
             //   1. The SCCs in the combinations must be physically connected to each other and connected to their parent.
             //   2. or if they are not connected to each other, each of them individually needs to be connected to the parent.
-            var combinationsHash = new HashSet<HashSet<hyperarc>>();
+            var combinationsHash = new List<List<hyperarc>>();
             var combinations = new List<List<hyperarc>>();
             combinations.AddRange(freeSCCs.Select(ini => new List<hyperarc> {ini}));
             
@@ -166,9 +166,76 @@ namespace Assembly_Planner
                 merged = MergeConnectedListOfHyperarcs(merged);
             }
             foreach (var com in combinations)
-                combinationsHash.Add(new HashSet<hyperarc>(com));
+                combinationsHash.Add(new List<hyperarc>(com));
 
             return combinationsHash;
+        }
+
+        private static List<List<hyperarc>> CombinationsCreatorPro3(designGraph assemblyGraph, List<hyperarc> freeSCCs, List<hyperarc> parents)
+        {
+            // ACCEPTABLE COMBINATIONS:
+            // 
+            var finalCombination = new List<List<hyperarc>>();
+            var dic = new Dictionary<hyperarc,List<hyperarc>>();
+            foreach (var scc in freeSCCs)
+                dic.Add(scc, ScrewedToScc(assemblyGraph, freeSCCs, scc));
+
+            var generated = new Queue<List<hyperarc>>();
+            var screwed = new List<hyperarc>();
+            foreach (var parent in parents)
+                screwed.AddRange(ScrewedToScc(assemblyGraph, freeSCCs, parent));
+            var combin = CombinationsCreator(screwed);
+            foreach (var comb in combin)
+                generated.Enqueue(comb);
+
+            while (generated.Any())
+            {
+                var opt = generated.Dequeue();
+                if (!(finalCombination.Any(hys => hys.All(opt.Contains) && opt.All(hys.Contains))))
+                    finalCombination.Add(opt);
+                var screwedToOption = ScrewedToOption(dic, opt);
+                var combination = CombinationsCreator(screwedToOption);
+                foreach (var com in combination)
+                {
+                    var merged = new List<hyperarc>(com);
+                    merged.AddRange(opt);
+                    if (generated.Any(hys => hys.All(merged.Contains) && merged.All(hys.Contains)))
+                        continue;
+                    generated.Enqueue(merged);
+                }
+            }
+
+            return finalCombination;
+        }
+
+        private static List<hyperarc> ScrewedToOption(Dictionary<hyperarc, List<hyperarc>> dic, List<hyperarc> opt)
+        {
+            // screwed to Opt and not included in Opt
+            var screwed = new List<hyperarc>();
+            foreach (var hy in opt)
+            {
+                foreach (var scrs in dic[hy])
+                {
+                    if (opt.Contains(scrs) || screwed.Contains(scrs)) continue;
+                    screwed.Add(scrs);
+                }
+            }
+            return screwed;
+        }
+
+        private static List<hyperarc> ScrewedToScc(designGraph assemblyGraph, List<hyperarc> freeSccs, hyperarc hy)
+        {
+            var screwed = new List<hyperarc>();
+            foreach (var scc in freeSccs)
+            {
+                if (
+                    assemblyGraph.arcs.Any(
+                        a =>
+                            ((hy.nodes.Contains(a.From) && scc.nodes.Contains(a.To)) ||
+                            (scc.nodes.Contains(a.From) && hy.nodes.Contains(a.To))) && (a.localVariables.Contains(DisConstants.BoltDirectionOfFreedom))))
+                    screwed.Add(scc);
+            }
+            return screwed;
         }
 
         private static List<List<hyperarc>> MergeConnectedListOfHyperarcs(List<List<hyperarc>> groupsOfHyperarcs)
