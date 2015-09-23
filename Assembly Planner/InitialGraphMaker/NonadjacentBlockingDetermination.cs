0a2ed3c4ee59355f7735b2b4d239e82f9a7d5a01
Prof. Matt Campbell <micampbell@users.noreply.github.com>
2015-09-23 09:20:14 -0700

    ray intersting with graph one more time.

--- Assembly Planner/InitialGraphMaker/NonadjacentBlockingDetermination.cs
+++ Assembly Planner/InitialGraphMaker/NonadjacentBlockingDetermination.cs
@@ -473,21 +473,30 @@ namespace Assembly_Planner
         public static bool RayIntersectsWithFace3(Ray ray, PolygonalFace face)
         {
             if (ray.Direction.dotProduct(face.Normal) > -0.06) return false;
-            var point = face.Vertices[0].Position;
-            var w = new double[] { ray.Position[0] - point[0], ray.Position[1] - point[1], ray.Position[2] - point[2] };
+            var w = ray.Position.subtract(face.Vertices[0].Position);
             var s1 = (face.Normal.dotProduct(w)) / (face.Normal.dotProduct(ray.Direction));
             //var v = new double[] { w[0] + s1 * ray.Direction[0] + point[0], w[1] + s1 * ray.Direction[1] + point[1], w[2] + s1 * ray.Direction[2] + point[2] };
-            var v = new double[] { ray.Position[0] - s1 * ray.Direction[0], ray.Position[1] - s1 * ray.Direction[1], ray.Position[2] - s1 * ray.Direction[2] };
-            foreach (var corner in face.Vertices)
-            {
-                var otherCorners = face.Vertices.Where(ver => ver != corner).ToList();
-                var v1 = otherCorners[0].Position.subtract(corner.Position);
-                var v2 = otherCorners[1].Position.subtract(corner.Position);
-                var v0 = v.subtract(corner.Position);
-                if (v1.crossProduct(v0).dotProduct(v2.crossProduct(v0)) > -0.15) //   -0.09 
-                    return false;
-            }
-            return true;
+            var pointOnTrianglesPlane = new [] { ray.Position[0] - s1 * ray.Direction[0], ray.Position[1] - s1 * ray.Direction[1], ray.Position[2] - s1 * ray.Direction[2] };
+            var v0 = face.Vertices[0].Position.subtract(pointOnTrianglesPlane);
+            var v1 = face.Vertices[1].Position.subtract(pointOnTrianglesPlane);
+            var v2 = face.Vertices[2].Position.subtract(pointOnTrianglesPlane);
+            var crossv0v1 = v0.crossProduct(v1);
+            var crossv1v2 = v1.crossProduct(v2);
+            var dot = crossv0v1.dotProduct(crossv1v2);
+            if (dot < 0.0) return false;
+            var crossv2v0 = v2.crossProduct(v0);
+            dot = crossv1v2.dotProduct(crossv2v0);
+            return (dot >= 0.0);
+            //foreach (var corner in face.Vertices)
+            //{
+            //    var otherCorners = face.Vertices.Where(ver => ver != corner).ToList();
+            //    var v1 = otherCorners[0].Position.subtract(corner.Position);
+            //    var v2 = otherCorners[1].Position.subtract(corner.Position);
+            //    var v0 = v.subtract(corner.Position);
+            //    if (v1.crossProduct(v0).dotProduct(v2.crossProduct(v0)) > -0.15) //   -0.09 
+            //        return false;
+            //}
+            //return true;
 
         }
 
