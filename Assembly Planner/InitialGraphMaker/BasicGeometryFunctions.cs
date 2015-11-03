a54c6b80dcd49f34c016b11ba4b43ea26d19c7ce
nima1367 <nima1367@users.noreply.github.com>
2015-11-02 17:42:05 -0800

    Update on "Polynomial Trend Detector" function

--- Assembly Planner/InitialGraphMaker/BasicGeometryFunctions.cs
+++ Assembly Planner/InitialGraphMaker/BasicGeometryFunctions.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using AssemblyEvaluation;
+using StarMathLib;
+using TVGL;
 
 namespace Assembly_Planner
 {
@@ -14,6 +17,48 @@ namespace Assembly_Planner
                 Math.Sqrt((Math.Pow(vertex1[0] - vertex2[0], 2)) +
                           (Math.Pow(vertex1[1] - vertex2[1], 2)) +
                           (Math.Pow(vertex1[2] - vertex2[2], 2)));
+        }
+
+        public static bool RayIntersectsWithFace(Ray ray, PolygonalFace face, out double[] hittingPoint)
+        {
+            hittingPoint = null;
+            if (ray.Direction.dotProduct(face.Normal) > -0.06) return false;
+            var w = ray.Position.subtract(face.Vertices[0].Position);
+            var s1 = (face.Normal.dotProduct(w)) / (face.Normal.dotProduct(ray.Direction));
+            //var v = new double[] { w[0] + s1 * ray.Direction[0] + point[0], w[1] + s1 * ray.Direction[1] + point[1], w[2] + s1 * ray.Direction[2] + point[2] };
+            //var v = new double[] { ray.Position[0] - s1 * ray.Direction[0], ray.Position[1] - s1 * ray.Direction[1], ray.Position[2] - s1 * ray.Direction[2] };
+            var pointOnTrianglesPlane = new[] { ray.Position[0] - s1 * ray.Direction[0], ray.Position[1] - s1 * ray.Direction[1], ray.Position[2] - s1 * ray.Direction[2] };
+            hittingPoint = pointOnTrianglesPlane;
+            var v0 = face.Vertices[0].Position.subtract(pointOnTrianglesPlane);
+            var v1 = face.Vertices[1].Position.subtract(pointOnTrianglesPlane);
+            var v2 = face.Vertices[2].Position.subtract(pointOnTrianglesPlane);
+            var crossv0v1 = v0.crossProduct(v1);
+            var crossv1v2 = v1.crossProduct(v2);
+            var dot = crossv0v1.dotProduct(crossv1v2);
+            if (dot < 0.0) return false;
+            var crossv2v0 = v2.crossProduct(v0);
+            dot = crossv1v2.dotProduct(crossv2v0);
+            return (dot >= 0.0);
+        }
+
+        public static bool RayIntersectsWithFace(Ray ray, PolygonalFace face)
+        {
+            if (ray.Direction.dotProduct(face.Normal) > -0.06) return false;
+            var w = ray.Position.subtract(face.Vertices[0].Position);
+            var s1 = (face.Normal.dotProduct(w)) / (face.Normal.dotProduct(ray.Direction));
+            //var v = new double[] { w[0] + s1 * ray.Direction[0] + point[0], w[1] + s1 * ray.Direction[1] + point[1], w[2] + s1 * ray.Direction[2] + point[2] };
+            //var v = new double[] { ray.Position[0] - s1 * ray.Direction[0], ray.Position[1] - s1 * ray.Direction[1], ray.Position[2] - s1 * ray.Direction[2] };
+            var pointOnTrianglesPlane = new[] { ray.Position[0] - s1 * ray.Direction[0], ray.Position[1] - s1 * ray.Direction[1], ray.Position[2] - s1 * ray.Direction[2] };
+            var v0 = face.Vertices[0].Position.subtract(pointOnTrianglesPlane);
+            var v1 = face.Vertices[1].Position.subtract(pointOnTrianglesPlane);
+            var v2 = face.Vertices[2].Position.subtract(pointOnTrianglesPlane);
+            var crossv0v1 = v0.crossProduct(v1);
+            var crossv1v2 = v1.crossProduct(v2);
+            var dot = crossv0v1.dotProduct(crossv1v2);
+            if (dot < 0.0) return false;
+            var crossv2v0 = v2.crossProduct(v0);
+            dot = crossv1v2.dotProduct(crossv2v0);
+            return (dot >= 0.0);
         }
     }
 }
--- Assembly Planner/InitialGraphMaker/BoltAndGearDetector/FastenerPolynomialTrend.cs
+++ Assembly Planner/InitialGraphMaker/BoltAndGearDetector/FastenerPolynomialTrend.cs
@@ -4,8 +4,10 @@ using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using AssemblyEvaluation;
 using StarMathLib;
 using TVGL;
+using Vertex = TVGL.Vertex;
 
 namespace Assembly_Planner
 {
@@ -19,14 +21,38 @@ namespace Assembly_Planner
             PolygonalFace triangle1;
             PolygonalFace triangle2;
             BoltAndGearDetection.LongestPlaneOfObbDetector(obb, out triangle1, out triangle2);
-            // take the middle point of the smallest edge of each triangle. 
-            // Generate k points between them with equal distances. 
-            // Generate rays using generate points. 
+            // 1. Take the middle point of the smallest edge of each triangle. 
+            // 2. Generate k points between them with equal distances. 
+            // 3. Generate rays using generate points. 
             var shortestEdgeMidPoint1 = ShortestEdgeMidPointOfTriangle(triangle1);
             var shortestEdgeMidPoint2 = ShortestEdgeMidPointOfTriangle(triangle2);
-            // Generate k points between these two midpoints:
+            
             var kPointsBetweenMidPoints = KpointBtwMidPointsGenerator(shortestEdgeMidPoint1, shortestEdgeMidPoint2, 100);
+
+            var distancePointToSolid = PointToSolidDistanceCalculator(solid, kPointsBetweenMidPoints,
+                triangle1.Normal.multiply(-1.0));
+
+
             return false;
+        }
+
+        private static List<double> PointToSolidDistanceCalculator(TessellatedSolid solid,
+            List<double[]> kPointsBetweenMidPoints, double[] vector)
+        {
+            var distList = new List<double>();
+            foreach (var point in kPointsBetweenMidPoints)
+            {
+                var ray = new Ray(new AssemblyEvaluation.Vertex(point), new Vector(vector));
+                foreach (var face in solid.Faces)
+                {
+                    double[] hittingPoint;
+                    if (!BasicGeometryFunctions.RayIntersectsWithFace(ray, face, out hittingPoint))
+                        continue;
+                    distList.Add(BasicGeometryFunctions.DistanceBetweenTwoVertices(hittingPoint, point));
+                    break;
+                }
+            }
+            return distList;
         }
 
         private static List<double[]> KpointBtwMidPointsGenerator(double[] shortestEdgeMidPoint1, double[] shortestEdgeMidPoint2, int k)
