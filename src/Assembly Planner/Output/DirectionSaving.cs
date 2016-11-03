d70ac20c62924493a409c279c62682889a37d774
Braxton Cuneo <braxtoncuneo@users.noreply.github.com>
2016-11-03 14:10:53 -0700

    Commit For Merge

---
+++ src/Assembly Planner/Output/DirectionSaving.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GraphSynth.Representation;
+using System.Xml.Serialization;
+using System.IO;
+
+
+namespace Assembly_Planner
+{
+    public class DirectionSaveStructure
+    {
+        public List<double[]> Directions;
+
+        public List<arc> arcs;
+
+    }
+}
