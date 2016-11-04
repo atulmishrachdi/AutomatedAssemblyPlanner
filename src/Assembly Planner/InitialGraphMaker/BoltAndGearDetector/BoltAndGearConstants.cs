e44c8fe632c3ba30def1abf359793dafc682679a
Braxton Cuneo <braxtoncuneo@users.noreply.github.com>
2016-11-04 16:15:22 -0700

    Added Name Recognition to Fastener Detection

---
+++ src/Assembly Planner/InitialGraphMaker/BoltAndGearDetector/BoltAndGearConstants.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assembly_Planner
+{
+    internal class BoltAndGearConstants
+    {
+        public const double ConePortion = 0.35;
+        public static double ConeAreaPortion = 0.25;
+        public static int TriabglesInTheGearSideFaces = 100;
+        public static int AcceptableNumberOfDenseEdges = 150;
+        public static double SmoothAngle = 0.9; // 25.8 degree
+        public static int GearTeeth = 15;
+    }
+}
---
+++ src/Assembly Planner/PostProcessor/UpdatePostProcessor.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AssemblyEvaluation;
+
+namespace Assembly_Planner
+{
+    class UpdatePostProcessor
+    {
+
+        internal static void BuildSuccedingTaskDictionary(SubAssembly subAssembly, List<string> successors)
+        {
+            //if (subAssembly == null) return;
+            if (subAssembly.PartNames.Count == 1) return;
+            OptimalOrientation.SucTasks.Add(subAssembly.Name, successors);
+
+            var subSubAssembly = subAssembly.Install.Moving;
+            var subSuccessors = new List<string>(successors);
+            subSuccessors.Add(subAssembly.Name);
+            BuildSuccedingTaskDictionary(subSubAssembly as SubAssembly, subSuccessors);
+            subSubAssembly = subAssembly.Install.Reference;
+            subSuccessors = new List<string>(successors);
+            subSuccessors.Add(subAssembly.Name);
+            BuildSuccedingTaskDictionary(subSubAssembly as SubAssembly, subSuccessors);
+        }
+
+        internal static void BuildingInstallationTaskDictionary(SubAssembly subAssembly)
+        {
+            if (subAssembly.PartNames.Count == 1) return;
+            OptimalOrientation.InstTasks.Add(subAssembly.Name, subAssembly);
+            BuildingInstallationTaskDictionary(subAssembly.Install.Moving as SubAssembly);
+            BuildingInstallationTaskDictionary(subAssembly.Install.Reference as SubAssembly);
+        }
+
+        internal static void BuildingListOfReferencePreceedings(SubAssembly subAssembly)
+        {
+            //if (subAssembly == null) return;
+            if (subAssembly.PartNames.Count == 1) return;
+            OptimalOrientation.RefPrec.Add(subAssembly);
+            if (subAssembly.Install.Moving.PartNames.Count > 1)
+                OptimalOrientation.Movings.Add(subAssembly.Install.Moving as SubAssembly);
+            BuildingListOfReferencePreceedings(subAssembly.Install.Reference as SubAssembly);
+        }
+
+    }
+}
