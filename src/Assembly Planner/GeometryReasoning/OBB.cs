60564d579681a569b2da7f6192f106a596b28666
Nima <nima1367@users.noreply.github.com>
2016-11-07 11:47:02 -0800

    A minor change in PC to get rid of some unnecessary code

---
+++ src/Assembly Planner/GeometryReasoning/OBB.cs
@@ -0,0 +1,130 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Versioning;
+using StarMathLib;
+using TVGL;
+
+namespace Assembly_Planner
+{
+    public class OBB
+    {
+        // There are three different approaches to build the Oriented Bounding Box.
+        //   1. Using points
+        //   2. Using triangles
+        //   3. Using convex hull
+        // I will only implement the "using points" method here.
+
+        internal static BoundingBox BuildUsingPoints(List<Vertex> points)
+        {
+            var mu = new[] {0.0, 0.0, 0.0};
+            var C = new double[3, 3];
+            // loop over the points to find the mean point location
+            foreach (var point in points)
+                mu = mu.add(point.Position.divide((double) points.Count));
+
+            // loop over the points again to build the covariance matrix.  
+            // Note that we only have to build terms for the upper 
+            // trianglular portion since the matrix is symmetric
+            double cxx = 0.0, cxy = 0.0, cxz = 0.0, cyy = 0.0, cyz = 0.0, czz = 0.0;
+            foreach (var p in points.Select(point => point.Position))
+            {
+                cxx += p[0]*p[0] - mu[0]*mu[0];
+                cxy += p[0]*p[1] - mu[0]*mu[1];
+                cxz += p[0]*p[2] - mu[0]*mu[2];
+                cyy += p[1]*p[1] - mu[1]*mu[1];
+                cyz += p[1]*p[2] - mu[1]*mu[2];
+                czz += p[2]*p[2] - mu[2]*mu[2];
+            }
+            // now build the covariance matrix
+            C[0, 0] = cxx;
+            C[0, 1] = cxy;
+            C[0, 2] = cxz;
+            C[1, 0] = cxy;
+            C[1, 1] = cyy;
+            C[1, 2] = cyz;
+            C[2, 0] = cxz;
+            C[2, 1] = cyz;
+            C[2, 2] = czz;
+            double[][] dirs;
+            double volume;
+            var verts = BuildFromCovarianceMatrix(C, points, out dirs, out volume);
+            return new BoundingBox {CornerVertices = verts.Select(v=> new Point(v)).ToArray(), Volume = volume, Directions = dirs};
+        }
+
+        internal static Vertex[] BuildFromCovarianceMatrix(double[,] C, List<Vertex> points, out double[][] eigenVecs, out double volume)
+        {
+            var eugVal = C.GetEigenValuesAndVectors(out eigenVecs);
+            var r = eigenVecs[0];
+            var u = eigenVecs[1];
+            var f = eigenVecs[2];
+            r.normalize();
+            u.normalize();
+            f.normalize();
+            // set the rotation matrix using the eigvenvectors
+            var mRot = new double[3][];
+            mRot[0] = new double[3];
+            mRot[1] = new double[3];
+            mRot[2] = new double[3];
+            mRot[0][0] = r[0];
+            mRot[0][1] = u[0];
+            mRot[0][2] = f[0];
+            mRot[1][0] = r[1];
+            mRot[1][1] = u[1];
+            mRot[1][2] = f[1];
+            mRot[2][0] = r[2];
+            mRot[2][1] = u[2];
+            mRot[2][2] = f[2];
+            var minVec = new[] {double.PositiveInfinity, double.PositiveInfinity, double.PositiveInfinity};
+            var maxVec = new[] {double.NegativeInfinity, double.NegativeInfinity, double.NegativeInfinity};
+            foreach (var point in points)
+            {
+                var p = point.Position;
+                var pPrime1 = p.dotProduct(r);
+                var pPrime2 = p.dotProduct(u);
+                var pPrime3 = p.dotProduct(f);
+                if (pPrime1 < minVec[0]) minVec[0] = pPrime1;
+                if (pPrime1 > maxVec[0]) maxVec[0] = pPrime1;
+                if (pPrime2 < minVec[1]) minVec[1] = pPrime2;
+                if (pPrime2 > maxVec[1]) maxVec[1] = pPrime2;
+                if (pPrime3 < minVec[2]) minVec[2] = pPrime3;
+                if (pPrime3 > maxVec[2]) maxVec[2] = pPrime3;
+            }
+            // set the center of the OBB to be the average of the 
+            // minimum and maximum, and the extents be half of the
+            // difference between the minimum and maximum
+            var center = (minVec.add(maxVec)).divide(2.0);
+            var pos = new[] {mRot[0].dotProduct(center), mRot[1].dotProduct(center), mRot[2].dotProduct(center)};
+            var extent = (maxVec.subtract(minVec)).divide(2.0);
+            var orientedBoundingBox = new[]
+            {
+                new Vertex(((pos.subtract(r.multiply(extent[0]))).subtract(u.multiply(extent[1]))).subtract(f.multiply(extent[2]))),
+                new Vertex(((pos.add(r.multiply(extent[0]))).subtract(u.multiply(extent[1]))).subtract(f.multiply(extent[2]))),
+                new Vertex(((pos.add(r.multiply(extent[0]))).subtract(u.multiply(extent[1]))).add(f.multiply(extent[2]))),
+                new Vertex(((pos.subtract(r.multiply(extent[0]))).subtract(u.multiply(extent[1]))).add(f.multiply(extent[2]))),
+                new Vertex(((pos.subtract(r.multiply(extent[0]))).add(u.multiply(extent[1]))).subtract(f.multiply(extent[2]))),
+                new Vertex(((pos.add(r.multiply(extent[0]))).add(u.multiply(extent[1]))).subtract(f.multiply(extent[2]))),
+                new Vertex(((pos.add(r.multiply(extent[0]))).add(u.multiply(extent[1]))).add(f.multiply(extent[2]))),
+                new Vertex(((pos.subtract(r.multiply(extent[0]))).add(u.multiply(extent[1]))).add(f.multiply(extent[2])))
+               
+            };
+            volume = 8 * extent[0] * extent[1] * extent[2];
+            var clockWise = points.Any(
+                p =>
+                    p.Position.subtract(orientedBoundingBox[0].Position)
+                        .dotProduct(
+                            ((orientedBoundingBox[1].Position.subtract(orientedBoundingBox[0].Position)).crossProduct(
+                                orientedBoundingBox[3].Position.subtract(orientedBoundingBox[0].Position))).normalize()) > 0.01);
+            if (!clockWise)
+            {
+                // Make it CW:
+                return new[]
+                {
+                    orientedBoundingBox[1], orientedBoundingBox[0], orientedBoundingBox[3], orientedBoundingBox[2],
+                    orientedBoundingBox[5], orientedBoundingBox[4], orientedBoundingBox[7], orientedBoundingBox[6]
+                };
+            }
+            return orientedBoundingBox;
+        }
+
+    }
+}
---
+++ src/webVis/jsstl.js
@@ -0,0 +1,234 @@
+// Code copied from Github repo of tonylukasavage (and somewhat modified)
+// License of this code, as stated on the repo is:
+//
+//    Do whatever you want with this code. I offer it without expectation or warranty. 
+//    No need to credit me in your project or source code. A digital high five would be
+//    nice, but is not required.
+//
+
+
+// Notes:
+// - STL file format: http://en.wikipedia.org/wiki/STL_(file_format)
+// - 80 byte unused header
+// - All binary STLs are assumed to be little endian, as per wiki doc
+/**
+*
+* Converts a given block of binary stl data (as an arraybuffer) to a threeJS representation
+* of the geometry. Function courtesy of 'tonylukasavage' from Github, who released this to
+* the public domain. 
+*
+* @method parseStlBinary
+* @for renderGlobal
+* @param {Arraybuffer} stl The binary stl data
+* @return {Object} threeJS geometry object
+* 
+*/
+var parseStlBinary = function(stl) {
+	var geo = new THREE.Geometry();
+	var dv = new DataView(stl, 80); // 80 == unused header
+	var isLittleEndian = true;
+	var triangles = dv.getUint32(0, isLittleEndian); 
+	// console.log('arraybuffer length:  ' + stl.byteLength);
+	// console.log('number of triangles: ' + triangles);
+	var offset = 4;
+	for (var i = 0; i < triangles; i++) {
+		// Get the normal for this triangle
+		var normal = new THREE.Vector3(
+			dv.getFloat32(offset, isLittleEndian),
+			dv.getFloat32(offset+4, isLittleEndian),
+			dv.getFloat32(offset+8, isLittleEndian)
+		);
+		offset += 12;
+		// Get all 3 vertices for this triangle
+		for (var j = 0; j < 3; j++) {
+			geo.vertices.push(
+				new THREE.Vector3(
+					dv.getFloat32(offset, isLittleEndian),
+					dv.getFloat32(offset+4, isLittleEndian),
+					dv.getFloat32(offset+8, isLittleEndian)
+				)
+			);
+			offset += 12
+		}
+		// there's also a Uint16 "attribute byte count" that we
+		// don't need, it should always be zero.
+		offset += 2;   
+		// Create a new face for from the vertices and the normal             
+		geo.faces.push(new THREE.Face3(i*3, i*3+1, i*3+2, normal));
+	}
+	// The binary STL I'm testing with seems to have all
+	// zeroes for the normals, unlike its ASCII counterpart.
+	// We can use three.js to compute the normals for us, though,
+	// once we've assembled our geometry. This is a relatively 
+	// expensive operation, but only needs to be done once.
+	geo.computeFaceNormals();
+	
+	return geo;
+};  
+
+
+
+/**
+*
+* Processes a given string to make it parsible for parseStl and returns
+* the results
+*
+* @method trim
+* @for renderGlobal
+* @param {String} str ASCII STL data
+* @return {String} processed string
+* 
+*/
+function trim (str) {
+	str = str.replace(/^\s+/, '');
+	for (var i = str.length - 1; i >= 0; i--) {
+		if (/\S/.test(str.charAt(i))) {
+			str = str.substring(0, i + 1);
+			break;
+		}
+	}
+	return str;
+}
+			
+
+			
+// Added this in to turn the input buffer into an actual string
+/**
+*
+* Converts an arraybuffer into a string of equivalent binary content
+* @method arrayToString
+* @for renderGlobal
+* @param {Arraybuffer} buf The arraybuffer
+* @return {String} 
+* 
+*/
+function arrayToString(buf) {
+	var pos=0;
+	var arr=new Uint8Array(buf);
+	var lim=arr.length;
+	var result="";
+	while(pos<lim){
+		result=result+String.fromCharCode(arr[pos]);
+		pos++;
+	}
+	return result;
+}
+
+
+
+/**
+*
+* Converts a given block of ASCII stl data (as an arraybuffer) to a threeJS representation
+* of the geometry. Function courtesy of 'tonylukasavage' from Github, who released this to
+* the public domain. 
+*
+* @method parseStl
+* @for renderGlobal
+* @param {Arraybuffer} stl The ASCII stl data
+* @return {Object} threeJS geometry object
+* 
+*/
+var parseStl = function(stl) {
+	
+	var state = '';
+	
+	stl=arrayToString(stl);
+	
+	
+	var lines = stl.split('\n');
+	var geo = new THREE.Geometry();
+	var name, parts, line, normal, done, vertices = [];
+	var vCount = 0;
+	stl = null;
+	for (var len = lines.length, i = 0; i < len; i++) {
+		if (done) {
+			break;
+		}
+		line = trim(lines[i]);
+		parts = line.split(' ');
+		switch (state) {
+			case '':
+				if (parts[0] !== 'solid') {
+					console.error(line);
+					console.error('Invalid state "' + parts[0] + '", should be "solid"');
+					return null;
+				} else {
+					name = parts[1];
+					state = 'solid';
+				}
+				break;
+			case 'solid':
+				if (parts[0] !== 'facet' || parts[1] !== 'normal') {
+					console.error(line);
+					console.error('Invalid state "' + parts[0] + '", should be "facet normal"');
+					return null;
+				} else {
+					normal = [
+						parseFloat(parts[2]), 
+						parseFloat(parts[3]), 
+						parseFloat(parts[4])
+					];
+					state = 'facet normal';
+				}
+				break;
+			case 'facet normal':
+				if (parts[0] !== 'outer' || parts[1] !== 'loop') {
+					console.error(line);
+					console.error('Invalid state "' + parts[0] + '", should be "outer loop"');
+					return null;
+				} else {
+					state = 'vertex';
+				}
+				break;
+			case 'vertex': 
+				if (parts[0] === 'vertex') {
+					geo.vertices.push(new THREE.Vector3(
+						parseFloat(parts[1]),
+						parseFloat(parts[2]),
+						parseFloat(parts[3])
+					));
+				} else if (parts[0] === 'endloop') {
+					geo.faces.push( new THREE.Face3( vCount*3, vCount*3+1, vCount*3+2, new THREE.Vector3(normal[0], normal[1], normal[2]) ) );
+					vCount++;
+					state = 'endloop';
+				} else {
+					console.error(line);
+					console.error('Invalid state "' + parts[0] + '", should be "vertex" or "endloop"');
+					return null;
+				}
+				break;
+			case 'endloop':
+				if (parts[0] !== 'endfacet') {
+					console.error(line);
+					console.error('Invalid state "' + parts[0] + '", should be "endfacet"');
+					return null;
+				} else {
+					state = 'endfacet';
+				}
+				break;
+			case 'endfacet':
+				if (parts[0] === 'endsolid') {
+					return geo;
+					done = true;
+				} else if (parts[0] === 'facet' && parts[1] === 'normal') {
+					normal = [
+						parseFloat(parts[2]), 
+						parseFloat(parts[3]), 
+						parseFloat(parts[4])
+					];
+					if (vCount % 1000 === 0) {
+						console.log(normal);
+					}
+					state = 'facet normal';
+				} else {
+					console.error(line);
+					console.error('Invalid state "' + parts[0] + '", should be "endsolid" or "facet normal"');
+					return null;
+				}
+				break;
+			default:
+				console.error('Invalid state "' + state + '"');
+				break;
+		}
+	}
+};
