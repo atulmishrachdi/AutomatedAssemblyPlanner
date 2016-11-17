c05ba392ebec643e8023d14b8f4497edda595586
Weifeng Huang <huangwe@onid.oregonstate.edu>
2016-11-17 15:33:55 -0800

    add online gp, speedup the evaluation

---
+++ Assembly Planner/GraphSynth.BaseClasses/Fastener.cs
@@ -0,0 +1,330 @@
+﻿// ***********************************************************************
+// Assembly         : Assembly Planner
+// Author           : 
+// Created          : 08-27-2015
+//
+// Last Modified By : Matt
+// Last Modified On : 08-27-2015
+// ***********************************************************************
+// <copyright file="Fastener.cs" company="">
+//     Copyright ©  2015
+// </copyright>
+// <summary></summary>
+// ***********************************************************************
+/// <summary>
+/// The BaseClasses namespace.
+/// </summary>
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Xml.Serialization;
+using Assembly_Planner.GraphSynth.BaseClasses;
+using GraphSynth.Representation;
+using StarMathLib;
+using TVGL;
+
+namespace Assembly_Planner
+{
+    /// <summary>
+    /// Enum FastenerTypeEnum
+    /// </summary>
+    public enum FastenerTypeEnum
+    {
+        /// <summary>
+        /// The bolt
+        /// </summary>
+        Bolt,
+        /// <summary>
+        /// The screw
+        /// </summary>
+        Screw,
+        /// <summary>
+        /// The other threaded post
+        /// </summary>
+        OtherThreadedPost,
+        /// <summary>
+        /// The pin
+        /// </summary>
+        Pin,
+        /// <summary>
+        /// The rivet
+        /// </summary>
+        Rivet,
+        /// <summary>
+        /// The clip
+        /// </summary>
+        Clip,
+        /// <summary>
+        /// The adhesive
+        /// </summary>
+        Adhesive,
+        /// <summary>
+        /// The weld
+        /// </summary>
+        Weld
+    }
+    public enum NutType
+    {
+        /// <summary>
+        /// The hex
+        /// </summary>
+        Hex,
+        /// <summary>
+        /// The wing
+        /// </summary>
+        Wing
+    }
+    public enum Tool
+    {
+        /// <summary>
+        /// The Hex Wrench
+        /// </summary>
+        HexWrench,
+        /// <summary>
+        /// The Allen or Hex socket
+        /// </summary>
+        Allen,
+        /// <summary>
+        /// The simple flat screw driver
+        /// </summary>
+        FlatBlade,
+        /// <summary>
+        /// The phillips blade screw driver, 4 sided
+        /// </summary>
+        PhillipsBlade,
+        /// <summary>
+        /// The power screw driver
+        /// </summary>
+        powerscrewdriver
+    }
+    /// <summary>
+    /// Class Fastener.
+    /// </summary>
+    public class Fastener : FastenerBase
+    {
+        /// <summary>
+        /// The overall length
+        /// </summary>
+        public double OverallLength;
+        /// <summary>
+        /// The engaged length
+        /// </summary>
+        public double EngagedLength;
+        /// <summary>
+        /// The diameter
+        /// </summary>
+        public double Diameter;
+        /// <summary>
+        /// The access position
+        /// </summary>
+        public double[] AccessPosition;
+        /// <summary>
+        /// The access direction
+        /// </summary>
+        public double[] AccessDirection;
+        /// <summary>
+        /// The access cylinder radius
+        /// </summary>
+        public double AccessCylinderRadius;
+        /// <summary>
+        /// The fastener type
+        /// </summary>
+        public FastenerTypeEnum FastenerType;
+        /// <summary>
+        /// The number of threads if available
+        /// </summary>
+        public int NumberOfThreads;
+        /// <summary>
+        /// The nuts if available
+        /// </summary>
+        public List<Nut> Nuts;
+        /// <summary>
+        /// The washers if available
+        /// </summary>
+        public List<Washer> Washer;
+        /// <summary>
+        /// The Parts Locked By Fastener
+        /// </summary>
+        public List<int> PartsLockedByFastener;
+
+        /// <summary>
+        /// for every part that is locked by this fastener, (if they are more than 2), I add one triangle that is touching the
+        /// surface of the fastener. The order is the same as the order of the parts added to "PartsLockedByFastener". I can
+        /// use this in order to understand which part is on the top, which one is on the bottom. In other words to understand
+        /// the order of the PartsLockedByFastener. This will help later to install fastener at the right moment.
+        /// </summary>
+        private List<PolygonalFace> TrianglesOnTheLockedParts;
+
+        internal static void AddFastenersInformation(designGraph assemblyGraph, Dictionary<string, List<TessellatedSolid>> solidsNoFastener,
+            Dictionary<TessellatedSolid, List<PrimitiveSurface>> solidPrimitive)
+        {
+            var counter = 0;
+            foreach (var fastener in FastenerDetector.Fasteners)
+            {
+                counter++;
+                var lockedByTheFastener = PartsLockedByTheFastenerFinder(fastener.Solid, solidsNoFastener, solidPrimitive);
+                AddFastenerToArc(assemblyGraph, lockedByTheFastener, fastener);
+            }
+            // So, by this point, if there is a fastener between two or more parts, a new local variable
+            // is added to their arc which shows the direction of freedom of the fastener.
+            // So if I want to seperate two parts or two subassemblies, now I know that there is a 
+            // fastener holding them to each other. And I also know the removal direction of the fastener
+
+            // There is still a possibility here: if any of the potential fasteners are holding 2 or more parts
+            // The point is that they can be either a washer, nut or fastener. But if it is a fastener, I need 
+            // to find the parts that it's holding and add it to their arc
+            foreach (var possible in FastenerDetector.PotentialFastener.Keys)
+            {
+                counter++;
+                var locked = PartsLockedByTheFastenerFinder(possible, solidsNoFastener, solidPrimitive);
+                if (locked.Count < 2)
+                {
+                    if (locked.Count == 1)
+                    {
+                        var comp = (Component)assemblyGraph[locked[0]];
+                        var pin = new Fastener()
+                        {
+                            RemovalDirection =
+                                FastenerDetector.RemovalDirectionFinderUsingObb(possible,
+                                    BoundingGeometry.OrientedBoundingBoxDic[possible]),
+                            Solid = possible,
+                            Diameter = BoundingGeometry.BoundingCylinderDic[possible].Radius
+                        };
+                        if (comp.Pins == null) comp.Pins = new List<Fastener>();
+                        comp.Pins.Add(pin);
+                    }
+                }
+                PolygonalFace topPlane = null;
+                var fastener = new Fastener()
+                {
+                    RemovalDirection =
+                        FastenerDetector.RemovalDirectionFinderUsingObbWithTopPlane(possible,
+                            BoundingGeometry.OrientedBoundingBoxDic[possible], out topPlane),
+                    Solid = possible,
+                    Diameter = BoundingGeometry.BoundingCylinderDic[possible].Radius
+                };
+                AddFastenerToArc(assemblyGraph, locked, fastener);
+                /*(if (fastener.PartsLockedByFastener.Count > 2)
+                    // if there are more than 2 parts locked by the fastener, sort them based on their distance to the top plane of the fastener
+                {
+
+                }*/
+            }
+        }
+
+        private static void AddFastenerToArc(designGraph assemblyGraph, List<string> lockedByTheFastener, Fastener fastener)
+        {
+            fastener.PartsLockedByFastener = new List<int>();
+            foreach (
+                Connection connection in
+                    assemblyGraph.arcs.Where(
+                        a => lockedByTheFastener.Contains(a.From.name) && lockedByTheFastener.Contains(a.To.name))
+                        .ToList())
+            {
+                if (lockedByTheFastener.Count > 2)
+                {
+                    foreach (var solid in lockedByTheFastener)
+                    {
+                        var nodeInd =
+                            assemblyGraph.nodes.IndexOf(assemblyGraph.nodes.Where(n => n.name == solid).ToList()[0]);
+                        if (fastener.PartsLockedByFastener.Contains(nodeInd)) continue;
+                        fastener.PartsLockedByFastener.Add(nodeInd);
+                    }
+                }
+                connection.Fasteners.Add(fastener);
+            }
+        }
+
+        private static List<string> PartsLockedByTheFastenerFinder(TessellatedSolid fastener, Dictionary<string, List<TessellatedSolid>> solidsNoFastener,
+            Dictionary<TessellatedSolid, List<PrimitiveSurface>> solidPrimitive)
+        {
+            var lockedByTheFastener = new List<string>();
+            foreach (var subAssem in solidsNoFastener)
+            {
+                foreach (var solid in subAssem.Value)
+                {
+                    // This has a way simpler blocking determination code. Check it out:
+                    if (!BlockingDetermination.BoundingBoxOverlap(fastener, solid)) continue;
+                    if (!BlockingDetermination.ConvexHullOverlap(fastener, solid)) continue;
+                    if (!BlockingDetermination.ProximityFastener(fastener, solid)) continue;
+                    //if (!FastenerPrimitiveOverlap(solidPrimitive[fastener], solidPrimitives)) continue;
+                    lockedByTheFastener.Add(subAssem.Key);
+                    break;
+                }
+            }
+            return lockedByTheFastener;
+        }
+
+        private static bool FastenerPrimitiveOverlap(List<PrimitiveSurface> fastenertPrimitives, List<PrimitiveSurface> solidPrimitives)
+        {
+            foreach (var primitiveA in fastenertPrimitives)
+            {
+                foreach (var primitiveB in solidPrimitives)
+                {
+                    if (primitiveA is Cylinder && primitiveB is Cylinder)
+                        if (PrimitivePrimitiveInteractions.CylinderCylinderOverlappingCheck((Cylinder)primitiveA, (Cylinder)primitiveB))
+                            return true;
+                    if (primitiveA is Cone && primitiveB is Cone)
+                        if (PrimitivePrimitiveInteractions.ConeConeOverlappingCheck((Cone)primitiveA, (Cone)primitiveB))
+                            return true;
+                }
+            }
+            return false;
+        }
+
+
+        /*private static List<int> BoltRemovalDirection(TessellatedSolid fastener)
+        {
+            var dir = new double[3];
+            var CvhSolid = new TessellatedSolid
+            {
+                Faces = fastener.ConvexHullFaces,
+                Edges = fastener.ConvexHullEdges
+            };
+
+            var solidPrim = TesselationToPrimitives.Run(CvhSolid);
+            var cones = solidPrim.Where(p => p is Cone).ToList();
+            if (cones.Count == 0)
+                throw Exception("If the part is Bolt or Screw, its CVH must contain Cone primitive");
+            var largestCone = new PrimitiveSurface();
+            var maxArea = 0.0;
+            foreach (var cone in cones)
+            {
+                if (cone.Area < maxArea) continue;
+                maxArea = cone.Area;
+                largestCone = cone;
+            }
+            var selectedCone = (Cone)largestCone;
+            dir = selectedCone.Axis.multiply(-1);
+            return NormalIndexInGlobalDirns(dir);
+        }*/
+    }
+
+    public class Nut : FastenerBase
+    {
+        /// <summary>
+        /// The overall length
+        /// </summary>
+        public double OverallLength;
+        /// <summary>
+        /// The number of threads if available
+        /// </summary>
+        public int NumberOfThreads;
+        /// <summary>
+        /// The nut type
+        /// </summary>
+        public NutType NutType;
+        /// <summary>
+        /// The washers if available
+        /// </summary>
+        public List<Washer> Washer;
+    }
+
+    public class Washer : FastenerBase
+    {
+
+    }
+
+}
---
+++ src/Assembly Planner/Evaluation/Constants.cs
@@ -0,0 +1,146 @@
+﻿
+using System;
+using System.IO;
+using System.Xml.Serialization;
+namespace AssemblyEvaluation
+{
+
+    public enum ConnectType
+    {
+        rectilinear = 1,
+        radial = 2,
+        loose_contact = 3,
+        unknown = 4,
+        strongly_connected = 5,
+        unknown_connection = 6
+    };
+
+    public enum InstallCharacterType
+    {
+        /// <summary>
+        /// The moving part is fully inside the reference part
+        /// </summary>
+        MovingIsInsideReference = 3,
+        /// <summary>
+        /// The moving part is on the outside or near the surface of reference part
+        /// </summary>
+        MovingIsOnOutsideOfReference = 2,
+        /// <summary>
+        /// The moving part and the reference part are similar in size
+        /// </summary>
+        MovingReferenceSimiliar = 1,
+        /// <summary>
+        /// The reference part and the moving are similiar but they should be switched (moving is harder to move).
+        /// </summary>
+        ReferenceMovingSimiliarSwitch = -1,
+        /// <summary>
+        /// The reference part is on the outside or near the surface of moving part - they should be switched.
+        /// </summary>
+        ReferenceIsOnOutsideOfMoving = -2,
+        /// <summary>
+        /// The reference part is fully inside the moving part - they should be switched.
+        /// </summary>
+        ReferenceIsInsideMoving = -3,
+        Unknown = 0
+    }
+
+
+    public class Constants
+    {
+
+        // Weights for differne evalutations
+        //
+        public static double Innerstabilityweight = 10;
+
+        public static double Installtimeweight = 0;
+        public static double Movingtimeweight = 0;
+        public static double Rotatetimeweight = 0;
+
+
+
+        public static Constants Values;
+        public string CADAssemblyFileName = "input.sat";
+        public string AssemblyXMLFileName = "input.xml";
+
+        /* ants for determining which subassembly is moving and which is the reference. */
+        public double CVXFormerFaceConfidence = 0.5;
+        public double CVXOnInsideThreshold = 0.05;
+        public double CVXOnOutsideThreshold = 0.5;
+
+
+
+        // Global ants for gxml
+        public int VISIBLE_DOF = -1000;         // visible DOF Tag
+        public int INVISIBLE_DOF = -2000;        // invisible DOF Tag
+        public int CLASH_LOCATION = -4000;      // clash location Tag
+        public int CONCENTRIC_DOF = -3000;     // concentric DOF Tag
+        public int EVALUATION = -1001;        // evaluation DOF Tag
+        public int BOXDIMENSIONS = -5000; //??
+        public int WEIGHT = -6000;
+        public int VOLUME = -6001;
+        public int CENTEROFMASS = -6005;
+        public int TRANSLATION = -7000;
+        public int ORDERSCORE = -8000;
+
+
+        public double MaxForce = 180; // this is in Newtons, similiar to 40 lbs.
+        public double StoppingDistance = 0.01; // in m (or 10 mm) distance to stop any object
+        public double MaxTravelSpeed = 1.0; // 1 meter per second
+        public double MaxInsertionSpeed = 0.2; // m per second
+        public double NearlyParallelFace = 0.03; //equivalent to 88.85 degrees //0.02
+        public double NearlyOnLine = 0.005; // about 178.8 degrees  0.0005
+        public double MaxPathForInfeasibleInstall = 999999.99999;
+        public double SameWithinError = 1e-9;
+        public double MinInterfaceSuccessRate = 0.8;
+        public double boltinsertSpeed = 2; // just a guess. the unit is mm/s
+
+        //public static bool ReadInEvaluationants(string filename = "Evaluationants.xml")
+        //{
+        //    try
+        //    {
+        //        XmlSerializer deserializer = new XmlSerializer(typeof(ants));
+        //        TextReader reader = new StreamReader(filename);
+        //        object obj = deserializer.Deserialize(reader);
+        //        ants XmlData = (ants)obj;
+        //        reader.Close();
+        //    }
+        //    catch (Exception e) { return false; }
+        //    return true;
+        //}
+        //public bool WriteEvaluationants(string filename = "Evaluationants.xml")
+        //{
+        //    try
+        //    {
+        //        XmlSerializer serializer = new XmlSerializer(typeof(ants));
+        //        using (TextWriter writer = new StreamWriter(filename))
+        //        {
+        //            serializer.Serialize(writer, this);
+        //        }
+        //    }
+        //    catch (Exception e) { return false; }
+        //    return true;
+        //}
+        public static void SaveXML(string filename)
+        {
+            using (var stream = new FileStream(filename, FileMode.Create))
+            {
+                XmlSerializer XML = new XmlSerializer(typeof(Constants));
+                XML.Serialize(stream, Constants.Values);
+            }
+        }
+
+        public static void LoadXML(string filename)
+        {
+            // try
+            //  {
+            XmlSerializer deserializer = new XmlSerializer(typeof(Constants));
+            TextReader reader = new StreamReader(filename);
+            Constants.Values = (Constants)deserializer.Deserialize(reader);
+            reader.Close();
+            //return class1;
+            // }
+            // catch (Exception e) { return false; }
+
+        }
+    }
+}
