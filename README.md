0bd2835bd868400f6d21d7d917789bd9eb34141b
Nima <nima1367@users.noreply.github.com>
2016-11-28 15:57:02 -0800

    More changes to make a graph connected if it's disconnected

---
+++ README.md
@@ -0,0 +1,44 @@
+# Automated Assembly Planning for Digital Manufacturing Commons #
+This repository represents a tool to automatically determine the best assembly plan for a given assembly. The basic method can be invoked:
+* as a plugin to ESI's IC.IDO Virtual Reality software
+* as a command line executable 
+* as a app for the Digital Manufacturing Commons.
+However, the first one has significantly changed given the functionality of IC.IDO and is no longer included here. This work was funded under DARPA AVM iFAB from 
+2012 to 2015 and then by DMDII from 2015 to present (set to expire on 3/31/2017).
+The work is being done primarily by the Design Engineering Lab at Oregon State University.
+
+## What does the application do? ##
+Given an CAD assembly representing a product to be assembled, the application searches for the best way to assemble it. Essentially, the application is a planning algorithm. It produces a plan along with estimates for timing for each step in the plan.
+
+
+## What does the application require (inputs/outputs)? ##
+###Inputs###
+A folder of shapes where each shape is positioned relative to one another in a global coordinate frame. The shapes can be in STL, PLY, 3MF or AMF file formats. 
+
+###Outputs###
+A single plan is created as an .xml file which describes a "treequence". A treequence (portmanteau of tree and sequence) is an assembly tree showing what parts come together in a particular order. 
+Given the timing associated with the actions and the accommodation for parallel assembly steps the treequence provides details of how and when to build parts of the system. This treequence is not
+particularly user-friendly. So, an external visualization has been created which shows the tree and an animation of the assembly process.
+
+## How do I get set up? ##
+1. Clone the repository 
+2. Open the file "Assembly Planner.sln" in visual studio. 
+3. Build. This should create a file in the root folder called "AssemblyPlanner.exe".
+
+## How to run the application ##
+1. The root directory is populated with a number of folders and binaries. All the dll's are there to support the main exe, AssemblyPlanner.exe. 
+2. In the folder, "workspace", place your shape files for your assembly (stls, plys, AMFs or 3MFs). Remove all other files.
+3. Now, run AssemblyPlanner.exe. A console window will open and begin sending status updates about the process. Do not close this window.
+4. This process will pause with the following statement, "Press enter once input parts table generated". At this time, go back to the root folder and open the file "partTable.html"
+5. In the newly opened web page, click on the "choose files" button in the upper-left corner of the screen and select the file named "parts_properties.xml" in the folder "workspace" (which should have just been generate by the program).
+6. In the resulting table, enter the density/mass information for each part and correct the fastener classification of each part, as needed, using the check boxes of each corresponding entry.
+7. Once the entire table has been filled out, click the button labeled "Render XML". This should cause a "download" link to appear next to the button. Click on this and move the downloaded file (which should be called "parts_properties2.xml") into the "workspace" folder.
+8. Navigate back to the console application and press enter.
+9. Once the process finishes, producing a notification giving the total time of the process, the console application may be closed.
+10. Open the file in the root folder named "render.html".
+11. In the newly opened web page, click on the "choose files" button and select all models in the "workspace" folder as well as the file labeled "solution.xml". Do not select any files beyond this. Then press "Open".
+12. The web page should be displaying a partially transparent panel overlay with a line of gibberish characters inside. This is a representation of the assembly. Click on the button to the left of this line of characters to show the sub-assemblies making up the final product. From there, two additional lines of symbols should appear, each with their own buttons. Once fully expanded, this interface should display the names of the parts as well as symbols that will be used for short-hand representation in parent assemblies. The show/hide button on the top-left of this may be used to toggle the visibility on this panel.
+13. To explore the assembly process, hide the overlay and press on the "mouse lock" button on the top-right of the page. Your mouse will disapear. This means that your mouse is locked. One may exit mouse lock at any time by pressing the escape key. While in mouse lock, the virtual camera linked to the display may be angled through mouse movement. Furthermore, the camera may be moved using the W,S,A,D, and Space keys. For ease of viewing, time progression in the assembly animation may be sped up through scrolling up on the mouse wheel or slowed down by scrolling down.
+
+## Contribution guidelines ##
+Current efforts are being completed by the Design Engineering Lab at Oregon State University. If you are interested in contributing contact Prof. Matt Campbell at matt<dot>campbell<at>oregonstate<dot>edu.
---
+++ src/Assembly Planner/DisassemblyProcess/Updates.cs
@@ -0,0 +1,249 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AssemblyEvaluation;
+using GraphSynth.Representation;
+using StarMathLib;
+using Assembly_Planner.GraphSynth.BaseClasses;
+using TVGL;
+using Constants = AssemblyEvaluation.Constants;
+
+namespace Assembly_Planner
+{
+    internal class Updates
+    {
+        internal static void ApplyChild(AssemblyCandidate child, option opt)
+        {
+            // The function removes hyperarcs with "SCC" or "Seperate" lables
+            for (var i = 0; i < child.graph.hyperarcs.Count; i++)
+            {
+                var hy = child.graph.hyperarcs[i];
+                if (hy.localLabels.Contains(DisConstants.SeperateHyperarcs) && hy.nodes.Any(n => opt.Nodes.Contains(n)))
+                {
+                    child.graph.removeHyperArc(hy);
+                    i--;
+                    continue;
+                }
+                if ((hy.localLabels.Contains(DisConstants.SeperateHyperarcs) &&
+                     !hy.nodes.Any(n => opt.Nodes.Contains(n))) || hy.localLabels.Contains(DisConstants.SingleNode))
+                    continue;
+
+                if (!hy.localLabels.Contains(DisConstants.Removable)) // Maybe all of them contain "Removable"
+                {
+                    child.graph.removeHyperArc(hy);
+                    i--;
+                }
+                else
+                {
+                    if (hy.localLabels.Contains(DisConstants.SCC))
+                        hy.localLabels.Remove(DisConstants.SCC);
+                }
+            }
+        }
+
+        internal static void UpdateGlobalDirections(List<int> globalDirPool)
+        {
+            // The function removes one of each parallel direction pair.
+            for (var i = 0; i < globalDirPool.Count - 1; i++)
+            {
+                var dir1 = DisassemblyDirections.Directions[globalDirPool[i]];
+                for (var j = i + 1; j < globalDirPool.Count; j++)
+                {
+                    var dir2 = DisassemblyDirections.Directions[globalDirPool[j]];
+                    if (Math.Abs(1 + dir1.dotProduct(dir2)) > 1e-3) continue;
+                    globalDirPool.RemoveAt(j);
+                    j--;
+                }
+            }
+        }
+
+        internal static List<hyperarc> UpdatePreceedings(List<hyperarc> Preceedings)
+        {
+            var list = new List<hyperarc>();
+            foreach (var preceeding in Preceedings.Where(p => !list.Contains(p)))
+                list.Add(preceeding);
+            return list;
+        }
+
+        internal static List<Component> AddSecondHyperToOption(AssemblyCandidate child, option opt)
+        {
+            foreach (
+                var sepHy in
+                    child.graph.hyperarcs.Where(
+                        a =>
+                            a.localLabels.Contains(DisConstants.SeperateHyperarcs) &&
+                            opt.Nodes.All(n => a.nodes.Contains(n)))) //
+                return sepHy.nodes.Where(n => !opt.Nodes.Contains(n)).Cast<Component>().ToList();
+            return null;
+        }
+
+        internal static void AddPartsProperties(string inputDir, designGraph assemblyGraph)
+        {
+            var reader = new StreamReader(File.OpenRead(@inputDir + "/information.csv"));
+            while (!reader.EndOfStream)
+            {
+                var line = reader.ReadLine();
+                var values = line.Split(',');
+                var i = 1;
+                var node = assemblyGraph.nodes.Cast<Component>().Where(n => n.name == Convert.ToString(values[0])).ToList()[0];
+                while (i < 9)
+                {
+                    if (Convert.ToDouble(values[i]) == Constants.Values.WEIGHT)
+                    {
+                        node.Mass = Convert.ToDouble(values[i + 1]);
+                        i+=2;
+                        continue;
+                    }
+                    if (Convert.ToDouble(values[i]) == Constants.Values.VOLUME)
+                    {
+                        node.Volume = Convert.ToDouble(values[i + 1]);
+                        i += 2;
+                        continue;
+                    }
+                    if (Convert.ToDouble(values[i]) == Constants.Values.CENTEROFMASS)
+                    {
+                        node.CenterOfMass = new[] { Convert.ToDouble(values[i + 1]), Convert.ToDouble(values[i + 2]), Convert.ToDouble(values[i + 3]) };
+                        i += 4;
+                        continue;
+                    }
+                }
+            }
+        }
+
+        internal static void UpdateChildGraph(AssemblyCandidate c, List<Component>[] install)
+        {
+            // This is happening in Evaluation
+            for (var j = 0; j < c.graph.hyperarcs.Count; j++)
+            {
+                var hy = c.graph.hyperarcs[j];
+                if (hy.localLabels.Contains(DisConstants.SeperateHyperarcs) ||
+                    hy.localLabels.Contains(DisConstants.SingleNode))
+                    continue;
+                c.graph.removeHyperArc(c.graph.hyperarcs[j]);
+                j--;
+            }
+
+            foreach (var list in install)
+            {
+                c.graph.addHyperArc(list.Cast<node>().ToList());
+                if (list.Count == 1)
+                    c.graph.hyperarcs[c.graph.hyperarcs.Count - 1].localLabels.Add(DisConstants.SingleNode);
+                else
+                    c.graph.hyperarcs[c.graph.hyperarcs.Count - 1].localLabels.Add(DisConstants.SeperateHyperarcs);
+            }
+        }
+
+
+        internal static bool EitherRefOrMovHasSeperatedSubassemblies(List<Component>[] install)
+        {
+            foreach (var subAsm in install)
+            {
+                var stack = new Stack<Component>();
+                var visited = new HashSet<Component>();
+                var globalVisited = new HashSet<Component>();
+                foreach (var node in subAsm.Where(n => !globalVisited.Contains(n)))
+                {
+                    stack.Clear();
+                    visited.Clear();
+                    stack.Push(node);
+                    while (stack.Count > 0)
+                    {
+                        var pNode = stack.Pop();
+                        visited.Add(pNode);
+                        globalVisited.Add(pNode);
+
+                        foreach (Connection pNodeArc in pNode.arcs.Where(a => a.GetType() == typeof(Connection)))
+                        {
+                            var otherNode = (Component)(pNodeArc.From == pNode ? pNodeArc.To : pNodeArc.From);
+                            if (visited.Contains(otherNode))
+                                continue;
+                            stack.Push(otherNode);
+                        }
+                    }
+                    if (visited.Count < subAsm.Count)
+                        return true;
+                }
+            }
+            return false;
+        }
+
+        internal static void RemoveRepeatedFasteners2(Connection a, designGraph graph)
+        {
+            //if (a.Fasteners.All(f => f.PartsLockedByFastener.Count < 3)) return;
+            for (var i = 0; i < a.Fasteners.Count; i++)
+            {
+                var fastener = a.Fasteners[i];
+                if (fastener.PartsLockedByFastener.Count < 2) continue;
+                var nodes = fastener.PartsLockedByFastener.Select(ind => graph.nodes[ind]).ToList();
+                foreach (
+                    Connection connection in
+                        graph.arcs.Cast<Connection>().Where(
+                            arc => arc != a && (nodes.Contains(arc.From) && nodes.Contains(arc.To))))
+                {
+                    connection.Fasteners.Remove(fastener);
+                }
+            }
+        }
+
+        internal static void RemoveRepeatedFastenersMain(Connection a, designGraph graph)
+        {
+            if (a.Fasteners.All(f => f.PartsLockedByFastener.Count < 3)) return;
+            var counter = a.localVariables.IndexOf(DisConstants.DirIndUpperBound) + 1;
+            var boltsInformation = new List<List<double>>();
+            while (counter < a.localVariables.Count)
+            {
+                var oneBoltInfo = new List<double>();
+                for (var i = counter; i < a.localVariables.Count; i++)
+                {
+                    oneBoltInfo.Add(a.localVariables[i]);
+                    if (i != a.localVariables.Count - 1 &&
+                        a.localVariables[i + 1] != DisConstants.BoltDirectionOfFreedom)
+                        continue;
+                    boltsInformation.Add(oneBoltInfo);
+                    counter += oneBoltInfo.Count;
+                    break;
+                }
+            }
+            foreach (var oneBoltInfo in boltsInformation)
+            {
+                if (!oneBoltInfo.Contains(DisConstants.IndexOfNodesLockedByFastenerL)) continue;
+                var l = oneBoltInfo.IndexOf(DisConstants.IndexOfNodesLockedByFastenerL);
+                var u = oneBoltInfo.IndexOf(DisConstants.IndexOfNodesLockedByFastenerU);
+                var nodes = new List<Component>();
+                for (var i = l + 1; i < u; i++)
+                    nodes.Add((Component)graph.nodes[(int)oneBoltInfo[i]]);
+                foreach (
+                    Connection arc in
+                        graph.arcs.Where(
+                            arc => arc is Connection && arc != a && (nodes.Contains(arc.From) && nodes.Contains(arc.To))))
+                {
+                    var counter2 = arc.localVariables.IndexOf(DisConstants.DirIndUpperBound) + 1;
+                    while (counter2 < arc.localVariables.Count)
+                    {
+                        var list = new List<double>();
+                        var removed = false;
+                        for (var i = 0; i < oneBoltInfo.Count; i++)
+                        {
+                            list.Add(arc.localVariables[i + counter2]);
+                            if (i + counter2 != arc.localVariables.Count - 1 &&
+                                arc.localVariables[i + counter2 + 1] != DisConstants.BoltDirectionOfFreedom)
+                                continue;
+                            if (oneBoltInfo.SequenceEqual(list))
+                            {
+                                arc.localVariables.RemoveRange(counter2, list.Count);
+                                removed = true;
+                                break;
+                            }
+                            counter2 += list.Count;
+                            break;
+                        }
+                        if (removed) break;
+                    }
+                }
+            }
+        }
+    }
+}
