6b8bb56a8f956988379eb8c9baa8d4ad5ada09f8
Braxton Cuneo <cuneob@oregonstate.edu>
2017-08-01 13:09:01 -0700

    Serialized Design Graphs now have the arc references of their nodes reconstructed prior to processing

--- src/Assembly Planner/Program.cs
+++ src/Assembly Planner/Program.cs
@@ -100,6 +100,7 @@ namespace Assembly_Planner
             state = new ProgramState();
             SetInputArguments(state, args);
             LoadState();
+			RefreshGraphArcs ();
 
             Solids = GetSTLs(state.inputDir);
             EnlargeTheSolid();
@@ -122,6 +123,7 @@ namespace Assembly_Planner
             state = new ProgramState();
 			ProgramState.Load("." + slash + "bin" + slash + "intermediate" + slash + "ProgramState.xml", ref state);
             LoadState();
+			RefreshGraphArcs ();
 
 			//$ Adding this so that bounding box related functionalities still work
 			BoundingGeometry.OrientedBoundingBoxDic = new Dictionary<TessellatedSolid, BoundingBox>();
@@ -146,6 +148,7 @@ namespace Assembly_Planner
 			state = new ProgramState();
 			ProgramState.Load("." + slash + "bin" + slash + "intermediate" + slash + "ProgramState.xml", ref state);
 			LoadState();
+			RefreshGraphArcs ();
 
 			//$ Adding this so that bounding box related functionalities still work
 			BoundingGeometry.OrientedBoundingBoxDic = new Dictionary<TessellatedSolid, BoundingBox>();
@@ -172,7 +175,8 @@ namespace Assembly_Planner
         {
             state = new ProgramState();
 			ProgramState.Load("." + slash + "bin" + slash + "intermediate" + slash + "ProgramState.xml", ref state);
-            LoadState();
+			LoadState();
+			RefreshGraphArcs ();
 
 			//$ Adding this so that bounding box related functionalities still work
 			BoundingGeometry.OrientedBoundingBoxDic = new Dictionary<TessellatedSolid, BoundingBox>();
@@ -392,15 +396,51 @@ namespace Assembly_Planner
             UpdateGraphArcs(reviewedArc);
         }
 
+
+		private static void RefreshGraphArcs(){
+
+			if (AssemblyGraph == null) {
+				return;
+			}
+			if (AssemblyGraph.arcs == null) {
+				return;
+			}
+			List<Connection> oldArcs = new List<Connection>(AssemblyGraph.arcs.Count);
+			foreach (Connection arc in AssemblyGraph.arcs) {
+				oldArcs.Add (arc);
+			}
+			foreach (Connection arc in oldArcs) {
+				AssemblyGraph.removeArc (arc);
+			}
+			foreach (Connection arc in oldArcs) {
+				node fromNode = AssemblyGraph.nodes.First(a => a.name == arc.XmlFrom);
+				node toNode = AssemblyGraph.nodes.First(a => a.name == arc.XmlTo);
+				AssemblyGraph.addArc(AssemblyGraph.nodes.First(a => a.name == arc.XmlFrom),
+					AssemblyGraph.nodes.First(a => a.name == arc.XmlTo),"",typeof(Connection));
+				var counterpart = (Connection)AssemblyGraph.arcs.Last ();
+				counterpart.FiniteDirections = arc.FiniteDirections;
+				counterpart.InfiniteDirections = arc.InfiniteDirections;
+				if (!fromNode.arcsFrom.Contains (counterpart)) {
+					throw new Exception ("Hollar");
+				}
+				if (!toNode.arcsTo.Contains (counterpart)) {
+					throw new Exception ("Hollar");
+				}
+			}
+
+		}
+
         private static void UpdateGraphArcs(List<arc> reviewedArc)
         {
+
             foreach (Connection arc in reviewedArc)
             {
-
 				//$ Incorperated the ability to add arcs, because otherwise resolving incomplete graphs
 				//  would be impossible
+
 				var counterpart =(Connection)
                     AssemblyGraph.arcs.FirstOrDefault(c => c.XmlFrom == arc.XmlFrom && c.XmlTo == arc.XmlTo);
+
 				if (counterpart == null) {
 					AssemblyGraph.addArc(AssemblyGraph.nodes.First(a => a.name == arc.XmlFrom),
 						AssemblyGraph.nodes.First(a => a.name == arc.XmlTo),"",typeof(Connection));
@@ -410,8 +450,10 @@ namespace Assembly_Planner
 						AssemblyGraph.removeArc(counterpart);
 					}
 				}
+
 				counterpart.FiniteDirections = AddDirections (arc.FiniteDirections);
 				counterpart.InfiniteDirections = AddDirections (arc.InfiniteDirections);
+
             }
         }
 
